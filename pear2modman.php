0edb566ba80f45492be3a76039792b2722c7a2b9
Daniel Sloof <d.sloof@drecomm.nl>
2012-06-04 11:41:48 +0200

    Initial commit.

---
+++ pear2modman.php
@@ -0,0 +1,201 @@
+<?php
+
+class ModmanGenerator
+{
+
+    protected $_packageContents  = null;
+    protected $_modmanDirectory  = '';
+    protected $_packageDirectory = '';
+
+    /**
+     * Sets PEAR package directory.
+     *
+     * @param string $packageDirectory
+     * @return void
+     */
+    public function ModmanGenerator($packageDirectory)
+    {
+        $this->_packageDirectory = $packageDirectory;
+    }
+
+    /**
+     * Reads PEAR package.xml and loads simplexml tree.
+     *
+     * @return string
+     * @throws Exception
+     */
+    protected function _getPackageContents()
+    {
+        if ($this->_packageContents === null) {
+            $packageFile = $this->_packageDirectory . '/package.xml';
+            if (!file_exists($packageFile)) {
+                throw new Exception('Could not find package file: ' . $packageFile);
+            }
+
+            $xml = simplexml_load_file($packageFile);
+            if ($xml) {
+                $this->_packageContents = current($xml->xpath('/package/contents'));
+            }
+        }
+        return $this->_packageContents;
+    }
+
+    /**
+     * Gets a valid package directory.
+     *
+     * @return string
+     * @throws Exception
+     */
+    protected function _getPackageDirectory()
+    {
+        if (empty($this->_packageDirectory)) {
+            throw new Exception('No valid package directory specified.');
+        }
+        return $this->_packageDirectory;
+    }
+
+    /**
+     * Gets, and optionally creates, the modman output directory.
+     *
+     * @return string
+     * @throws Exception
+     */
+    protected function _getModmanDirectory()
+    {
+        if (empty($this->_modmanDirectory)) {
+            $this->_modmanDirectory = $this->_packageDirectory . '/modman';
+            @mkdir($this->_modmanDirectory, 0755, true);
+            if (!file_exists($this->_modmanDirectory)) {
+                throw new Exception('Output modman directory does not exist.');
+            }
+        }
+        return $this->_modmanDirectory;
+    }
+
+    /**
+     * Gets the path to modman index file.
+     *
+     * @return string
+     */
+    protected function _getModmanFile()
+    {
+        return $this->_getModmanDirectory() . '/modman';
+    }
+
+    /**
+     * Writes an entry to the modman index file.
+     *
+     * @param string $from
+     * @param string $to
+     * @return string
+     */
+    protected function _writeModmanLine($from, $to)
+    {
+        return file_put_contents($this->_getModmanFile(), $from . ' ' . $to . PHP_EOL, FILE_APPEND);
+    }
+
+    /**
+     * Copies a folder from one location to another.
+     *
+     * @param string $from
+     * @param string $to
+     * @return string
+     */
+    protected function _copyFolder($from, $to)
+    {
+        /**
+         * TODO: Replace with some recursive copy() implementation.
+         */
+        return shell_exec(sprintf('cp -Rf %s %s', $from, $to));
+    }
+
+    /**
+     * Converts local code to modman.
+     *
+     * @param SimpleXMLElement $target
+     * @return \ModmanGenerator
+     */
+    protected function _handleLocalTarget($target)
+    {
+        /**
+         * Get package code path.
+         */
+        $namespaceNode = $target->children()->dir;
+        $originalCodePath = sprintf('app/code/local/%s/%s',
+            (string)$namespaceNode->attributes()->name,
+            (string)$namespaceNode->children()->dir->attributes()->name);
+
+        /**
+         * Copy it to modman target.
+         */
+        $this->_copyFolder(
+            $this->_getPackageDirectory() . '/' . $originalCodePath,
+            $this->_getModmanDirectory() . '/code');
+
+        /**
+         * Write entry in modman index file.
+         */
+        $this->_writeModmanLine('code', $originalCodePath);
+
+        return $this;
+    }
+
+    /**
+     * Converts bootstrap file to modman.
+     *
+     * @param SimpleXMLElement $target
+     * @return \ModmanGenerator
+     */
+    protected function _handleEtcTarget($target)
+    {
+        // STUB
+        return $this;
+    }
+
+    /**
+     * Converts design files to modman.
+     *
+     * @param SimpleXMLElement $target
+     * @return \ModmanGenerator
+     */
+    protected function _handleDesignTarget($target)
+    {
+        // STUB
+        return $this;
+    }
+
+    /**
+     * Starts the conversion of a specified PEAR package.
+     *
+     * @return \ModmanGenerator
+     * @throws Exception
+     */
+    public function start()
+    {
+        foreach ($this->_getPackageContents() as $target) {
+            $targetType = (string)$target->attributes()->name;
+            switch($targetType) {
+                case 'magelocal':
+                    $this->_handleLocalTarget($target);
+                    break;
+                case 'mageetc':
+                    $this->_handleEtcTarget($target);
+                    break;
+                case 'magedesign':
+                    $this->_handleDesignTarget($target);
+                    break;
+                default:
+                    throw new Exception(sprintf('Unhandle content target: %s', $targetType));
+            }
+        }
+        return $this;
+    }
+
+}
+
+$generator = new ModmanGenerator($argv[1]);
+try {
+    $generator->start();
+} catch (Exception $e) {
+    printf('Generator failed: %s' . PHP_EOL, $e->getMessage());
+}
---
+++ pear2modman.sh
@@ -0,0 +1,38 @@
+MAGE_PATH="./mage"
+CONVERT_PATH="convert/$1"
+
+# Check for command line arguments.
+if test -z "$1"
+then
+    echo "Usage: $0 <connect key>"
+    exit 0
+fi
+
+# Check for existence of mage file.
+if [ ! -f $MAGE_PATH ]
+then
+    echo "Mage file was not found."
+    exit 0
+fi
+
+# Delete temporary files.
+rm -rf $CONVERT_PATH && mkdir -p $CONVERT_PATH
+
+# Download and extract extension. 
+echo "Downloading $1..."
+PACKAGE_PATH=$(echo $($MAGE_PATH download community $1) | awk '{ print $3 }')
+
+# Check for existence of downloaded package.
+if [ "$PACKAGE_PATH" = "Package" ]
+then
+    echo "Could not find package - invalid key specified?"
+    rm -rf $CONVERT_PATH
+    exit 0
+fi
+echo "Extracting package to $CONVERT_PATH."
+tar -xzf $PACKAGE_PATH -C $CONVERT_PATH
+
+# Convert to modman. 
+echo "Converting..."
+php -f dl.php $CONVERT_PATH
+
